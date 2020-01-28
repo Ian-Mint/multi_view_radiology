0e18c6a3d5b0813555444c895d7da0158ae78b69
Ian Pegg <ianpegg9@gmail.com>
2020-01-28 13:03:49 -0800

    fix formatting in Data_Processing_Open_i

--- preprocessing/Data_Processing_Open_i.py
+++ preprocessing/Data_Processing_Open_i.py
@@ -7,31 +7,29 @@ Created on Fri Jan 24 15:43:44 2020
 
 import numpy as np
 import os
-import xml.etree.ElementTree as ET 
+import xml.etree.ElementTree as ET
 import pandas as pd
-
 
 Img_Findings = {}
 report_path = 'ecgen-radiology/'
 for filename in os.listdir(report_path):
     # create element tree object 
-    tree = ET.parse(report_path + filename) 
+    tree = ET.parse(report_path + filename)
 
     # get root element 
-    root = tree.getroot() 
-    
+    root = tree.getroot()
+
     # Report
     for i, items in enumerate(root.findall('MedlineCitation/Article/Abstract/AbstractText')):
-        if i == 2: 
+        if i == 2:
             findings = items.text
-    
+
     # Image
     for i, items in enumerate(root.findall('parentImage')):
         id = items.attrib['id']
         Img_Findings[id] = findings
-        
-        
-df = pd.DataFrame.from_dict(Img_Findings,orient='index', columns = ['Findings'])
+
+df = pd.DataFrame.from_dict(Img_Findings, orient='index', columns=['Findings'])
 df = df.dropna()
 df.reset_index(level=0, inplace=True)
 df.to_csv('Img_Report.csv')
