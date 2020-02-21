3a3b3ec0befb21cc615244cd7994336d761db167
root <ipegg@eng.ucsd.edu>
2020-02-21 23:28:52 +0000

    update model and data_loader

--- data_loader.py
+++ data_loader.py
@@ -9,21 +9,16 @@ import pandas as pd
 import numpy as np
 import os
 from torch.utils.data import Dataset
-from scipy.misc import imread, imresize
+import h5py
+import json
 import torch
 import nltk
-
 import config
 
-# Choose png or DICOM importer
-if config.img_extension == '.dcm':
-    import dicom_image as Image
-else:
-    from PIL import Image
 
 
 class OpenI(Dataset):
-    def __init__(self, root, vocab, img_name_report, split, transform=None):
+    def __init__(self, data_folder, data_name, split, transform=None):
         """Set the path for images, captions and vocabulary wrapper.
         
         Args:
@@ -33,85 +28,51 @@ class OpenI(Dataset):
             transform: image transformer.
         """
 
-        self.root = root
-        self.vocab = vocab
-        self.data = img_name_report
-        self.transform = transform
         self.split = split
+        assert self.split in {'TRAIN', 'VAL', 'TEST'}
+
+        # Open hdf5 file where images are stored
+        self.h = h5py.File(os.path.join(data_folder, self.split + '_IMAGES_' + data_name + '.hdf5'), 'r')
+        self.imgs = self.h['images']
+
+        # Captions per image
+        self.cpi = self.h.attrs['captions_per_image']
+
+        # Load encoded captions (completely into memory)
+        with open(os.path.join(data_folder, self.split + '_CAPTIONS_' + data_name + '.json'), 'r') as j:
+            self.captions = json.load(j)
+
+        # Load caption lengths (completely into memory)
+        with open(os.path.join(data_folder, self.split + '_CAPLENS_' + data_name + '.json'), 'r') as j:
+            self.caplens = json.load(j)
+
+        # PyTorch transformation pipeline for the image (normalizing, etc.)
+        self.transform = transform
+
+        # Total number of datapoints
+        self.dataset_size = len(self.captions)
+        
+        
+        
 
     def __getitem__(self, idx):
         """Returns one data pair (image and caption)."""
         
-        caption = self.data.iloc[idx]['Findings']
-        image_id = self.data.iloc[idx]['index']
-        vocab = self.vocab
-        
-        
-        img = imread(os.path.join(self.root, image_id + config.img_extension))
-        if len(img.shape) == 2:
-            img = img[:, :, np.newaxis]
-            img = np.concatenate([img, img, img], axis=2)
-        img = imresize(img, (256, 256))
-        img = img.transpose(2, 0, 1)
-        
-        img = torch.FloatTensor(img / 255.)
+        img = torch.FloatTensor(self.imgs[i // self.cpi] / 255.)
         if self.transform is not None:
             img = self.transform(img)
 
-        #image = Image.open(os.path.join(self.root, image_id + config.img_extension)).convert('RGB')
-        #if self.transform:
-        #    image = self.transform(image)
-        #image = image/255.
+        caption = torch.LongTensor(self.captions[i])
+        caplen = torch.LongTensor([self.caplens[i]])
 
-        tokens = nltk.tokenize.word_tokenize(str(caption).lower())
-        caption = []
-        caption.append(vocab('<start>'))
-        caption.extend([vocab(token) for token in tokens])
-        caption.append(vocab('<end>'))
-        target = torch.tensor(caption)
-        
-
-        return img, target
+        if self.split is 'TRAIN':
+            return img, caption, caplen
+        else:
+            # For validation of testing, also return all 'captions_per_image' captions to find BLEU-4 score
+            all_captions = torch.LongTensor(
+                self.captions[((i // self.cpi) * self.cpi):(((i // self.cpi) * self.cpi) + self.cpi)])
+            return img, caption, caplen, all_captions
 
     def __len__(self):
-        return len(self.data)
+        return self.dataset_size
 
-
-def collate_fn(data):
-    """Creates mini-batch tensors from the list of tuples (image, caption)."""
-
-    data.sort(key=lambda x: len(x[1]), reverse=True)
-    images, captions = zip(*data)
-
-    # Merge images (from tuple of 3D tensor to 4D tensor).
-    images = torch.stack(images, 0)
-
-    # Merge captions (from tuple of 1D tensor to 2D tensor).
-    lengths = [len(cap) for cap in captions]
-    targets = torch.zeros(len(captions), max(lengths)).long()
-
-    for idx, cap in enumerate(captions):
-        cap_end = lengths[idx]
-        targets[idx, :cap_end] = cap[:cap_end]
-
-    lengths = torch.tensor(lengths)
-
-    return images, targets, lengths
-
-
-def get_loader(root, vocab, img_report, transform, batch_size, shuffle, num_workers,split):
-    """Returns torch.utils.data.DataLoader for Open-i dataset."""
-
-    open_i = OpenI(root=root,
-                   vocab=vocab,
-                   img_name_report=img_report,
-                   split=split,
-                   transform=transform)
-
-    data_loader = torch.utils.data.DataLoader(dataset=open_i,
-                                              batch_size=batch_size,
-                                              shuffle=shuffle,
-                                              num_workers=num_workers,
-                                              collate_fn=collate_fn,
-                                              pin_memory = True)
-    return data_loader
